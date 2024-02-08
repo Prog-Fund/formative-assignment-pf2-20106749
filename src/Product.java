11670dbf7f2007808ed8c1b2b405d048e0d30975
Michael Louis Langan <20106749@mail.wit.ie>
2024-02-08 16:41:35 +0000

    Product
    # rewrite toString() to return a String as explained in the comments of toString()
        toString returns Y/N in place of true/false
        remove TODO comment
    
    # add validation to the Product Code field
        productCode between 1000 and 5000 inclusive, default 5000
    
    # add validation to the Unit Cost field
        unitCost any positive number - default to 1
    
    # add validation to the Product Name field
        productCode max length = 20 characters, default value is ""
        if the name is >20 chars, only store the first 20 characters
    
    # update constructor
        add missing statement to set inCurrentProductLine
        use methods in place of setting fields directly
            - setProductName()
            - setProductCode()
            - setUnitCost()

--- src/Product.java
+++ src/Product.java
@@ -5,13 +5,13 @@
  */
 public class Product {
 
-    private String productName ;    // max length = 20 characters, default value is "".
+    private String productName = "";    // max length = 20 characters, default value is "".
                                     // When constructor is called, if the name is >20 chars, you should
                                     // only store the first 20 characters (Hint: use substr())
 
-    private int productCode ;  // valid values 1000 - 5000 inclusive - default value is 5000
+    private int productCode = 5000;  // valid values 1000 - 5000 inclusive - default value is 5000
 
-    private double unitCost;  //valid values are any positive number - default to 1
+    private double unitCost = 1;  //valid values are any positive number - default to 1
 
     private boolean inCurrentProductLine;   // no validation required. Default
     /**
@@ -21,9 +21,10 @@ public class Product {
      * @param unitCost Unit cost of the product - valid values are any positive number
      */
     public Product(String productName, int productCode, double unitCost, boolean inCurrentProductLine) {
-       this.productName = productName;
-       this.productCode = productCode;
-       this.unitCost = unitCost;
+        setProductName(productName);
+        setProductCode(productCode);
+        setUnitCost(unitCost);
+        this.inCurrentProductLine = inCurrentProductLine;
     }
 
     //-------
@@ -65,21 +66,31 @@ public class Product {
      * @param productCode The new Product Code
      */
     public void setProductCode(int productCode) {
+        if (productCode >= 1000 && productCode <= 5000) {
             this.productCode = productCode;
+        }
     }
     /**
      * Updates the Product Name to the value passed as a parameter
      * @param productName The new Product Name
      */
     public void setProductName(String productName) {
-            this.productName = productName;
+        if (productName != null) {
+            if (productName.length() > 20) {
+                this.productName = productName.substring(0, 20);
+            } else {
+                this.productName = productName;
+            }
+        }
     }
     /**
      * Updates the Unit Cost to the value passed as a parameter
      * @param unitCost The new unit cost for the product
      */
     public void setUnitCost(double unitCost) {
+        if (unitCost >= 1) {
             this.unitCost = unitCost;
+        }
     }
     /**
      * Updates the boolean indicating whether the product is in the current product line or not.
@@ -98,7 +109,10 @@ public class Product {
     //  "Product description: Flatscreen TV  product code: 2000  unit cost: 1000 and currently in product line: Y"
 
     {
-        return "TO DO WRITE TOSTRING";
+        return
+                "Product description: " + productName +
+                "  product code: " + productCode +
+                "  unit cost: " + unitCost +
+                " and currently in product line: " + (inCurrentProductLine ? "Y" : "N");
     }
-
 }
